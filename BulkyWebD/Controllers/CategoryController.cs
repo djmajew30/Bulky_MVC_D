65a27142aafdaadd0678e0dda8d057ff081127de
djmajew30 <96849762+djmajew30@users.noreply.github.com>
2024-12-06 14:54:41 -0500

    Section 2 - Get all Categories using controller through applicationdbcontext and database

--- BulkyWebD/Controllers/CategoryController.cs
+++ BulkyWebD/Controllers/CategoryController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyWebD.Data;
+using BulkyWebD.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWebD.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        //ctor
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
